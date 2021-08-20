5319646e6b0e5d661c4c9cc03121831a2d222982
KRofLife <05410kira@gmail.com>
2021-08-20 13:35:07 +0000

    addfiles

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'cb8d459bb844b8f1d538ff96a878c4eea68efa1861a68baa4a5670656929af609b9ac4fda44305bc02e457aa4c227ada7cfdfa17b42bc6f79941a9ef8d1e0f6c'
